8f50e0e4fe9acdeaf3b625b47e228e7c6982d061
Paul Carlton <paul.b.carlton@gmail.com>
2015-12-04 15:41:42 -0800

    adding ultramvc.json for gulp, task management, and bower or personal dependency management

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,43 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	minifycss = require('gulp-minify-css'),
-	rename = require('gulp-rename'),
-	gutil = require('gulp-util');
+		sass = require('gulp-sass'),
+		autoprefixer = require('gulp-autoprefixer'),
+		minifycss = require('gulp-minify-css'),
+		rename = require('gulp-rename'),
+		gutil = require('gulp-util'),
+		jsmin = require('gulp-jsmin'),
+		uglify = require('gulp-uglify'),
+		fs = require('fs')
+;
+
+var ultra = JSON.parse(fs.readFileSync('ultramvc.json'));
 
 gulp.task('styles', function() {
-	gutil.log('Saving Default Styles');
-	gulp.src('./resources/sass/app.scss')
-			.pipe(sass('sass', {style:'expanded'}))
+	gutil.log('saving default styles');
+	gulp.src(ultra['styles']).pipe(sass('sass', {style:'expanded'}))
 			.on('error', sass.logError)
 			.pipe(gulp.dest('./resources/css'))
 			.pipe(minifycss())
 			.pipe(rename({suffix:'.min'}))
 			.pipe(gulp.dest('./resources/css'))
+
 });
 
-gulp.task('watch', function() {
-	gulp.watch('./resources/sass/*.scss', ['styles']);
-	gulp.watch('./resources/sass/**/*.scss', ['styles']);
+
+gulp.task('script', function() {
+	gulp.src(ultra['script'])
+			.pipe(uglify())
+			.pipe(jsmin())
+			.pipe(rename({suffix: '.min'}))
+			.pipe(gulp.dest('dist'))
+	;
 });
+
+
+gulp.task('watch', function() {
+	for (var j in ultra['watch']) {
+		gulp.watch(ultra['watch'][j], [j]);
+	}
+});
+
 
 gulp.task('default', ['watch']);
