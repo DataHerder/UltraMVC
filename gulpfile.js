153e8128b2e871fdc57c75dcdc7df935262e0ca0
Paul Carlton <paul.b.carlton@gmail.com>
2015-12-04 18:32:53 -0800

    scaffolding for automating dependency watches (like foundation, bootstrap, jquery bundles, etc...)

--- gulpfile.js
+++ gulpfile.js
@@ -6,38 +6,93 @@ var gulp = require('gulp'),
 		gutil = require('gulp-util'),
 		jsmin = require('gulp-jsmin'),
 		uglify = require('gulp-uglify'),
+		concat = require('gulp-concat'),
 		fs = require('fs')
-;
+		;
 
 var ultra = JSON.parse(fs.readFileSync('ultramvc.json'));
 
-gulp.task('styles', function() {
-	gutil.log('saving default styles');
-	gulp.src(ultra['styles']).pipe(sass('sass', {style:'expanded'}))
-			.on('error', sass.logError)
-			.pipe(gulp.dest('./resources/css'))
-			.pipe(minifycss())
-			.pipe(rename({suffix:'.min'}))
-			.pipe(gulp.dest('./resources/css'))
+watch_list = [];
+(function() {
+	for (var i = 0; i < ultra['watch']['styles'].length; i++) {
+		var style_data = ultra['watch']['styles'][i];
+		var read = style_data[0];
+		var write = style_data[1];
+		var watch = style_data[2];
+		var concat_styles = style_data[3];
+		watch_list.push(['styles' + i, watch]);
+		if (concat_styles) {
+			(function(read, write, concat_styles) {
+				gulp.task('styles' + i, function () {
+					gulp.src(read)
+							.pipe(concat(concat_styles))
+							.pipe(sass('sass', {style: 'expanded'}))
+							.on('error', sass.logError)
+							.pipe(gulp.dest(write))
+							.pipe(minifycss())
+							.pipe(rename({suffix: '.min'}))
+							.pipe(gulp.dest(write))
+					;
+				});
+			}(read, write, concat_styles));
+		} else {
+			(function(read, write) {
+				gulp.task('styles' + i, function() {
+					gulp.src(read)
+							.pipe(sass('sass', {style: 'expanded'}))
+							.on('error', sass.logError)
+							.pipe(gulp.dest(write))
+							.pipe(minifycss())
+							.pipe(rename({suffix:'.min'}))
+							.pipe(gulp.dest(write))
+					;
+				});
+			})(read, write);
+		}
+	}
 
-});
+	for (i = 0; i < ultra['watch']['scripts'].length; i++) {
+		var script_data = ultra['watch']['scripts'][i];
+		read = script_data[0];
+		write = script_data[1];
+		watch = script_data[2];
+		var concat_scripts = script_data[3];
 
+		watch_list.push(['scripts' + i, watch]);
+		if (concat_scripts) {
+			(function(read, write, concat_scripts) {
+				gulp.task('scripts' + i, function() {
+					gulp.src(read)
+							.pipe(concat(concat_scripts))
+							.pipe(gulp.dest(write))
+							.pipe(uglify())
+							.pipe(jsmin())
+							.pipe(rename({suffix: '.min'}))
+							.pipe(gulp.dest(write))
+					;
+				});
+			})(read, write, concat_scripts)
+		} else {
+			(function(read, write) {
+				gulp.task('scripts' + i, function() {
+					gulp.src(read)
+							.pipe(uglify())
+							.pipe(jsmin())
+							.pipe(rename({suffix: '.min'}))
+							.pipe(gulp.dest(write))
+					;
+				});
+			})(read, write);
+		}
+	}
+})();
 
-gulp.task('script', function() {
-	gulp.src(ultra['script'])
-			.pipe(uglify())
-			.pipe(jsmin())
-			.pipe(rename({suffix: '.min'}))
-			.pipe(gulp.dest('dist'))
-	;
-});
-
-
-gulp.task('watch', function() {
-	for (var j in ultra['watch']) {
-		gulp.watch(ultra['watch'][j], [j]);
+gulp.task('init', function() {
+	for (i = 0; i < watch_list.length; i++) {
+		console.log(watch_list[i]);
+		gulp.watch(watch_list[i][1], [watch_list[i][0]]);
 	}
 });
 
-
-gulp.task('default', ['watch']);
+// add your personal tasks here after 'init'
+gulp.task('default', ['init']);
