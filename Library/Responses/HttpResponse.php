ecd0602fa3201ad96b4178b7853f1572dd55b66d
Paul Carlton <paul.b.carlton@gmail.com>
2016-02-05 19:58:18 -0800

    took out warning if only passing error code

--- Library/Responses/HttpResponse.php
+++ Library/Responses/HttpResponse.php
@@ -5,14 +5,14 @@ namespace UltraMVC\Responses;
 final class HttpResponse extends Abstracts\ResponsesAbstract {
 
 	/**
-	 * Construct the response, defaults to 307
+	 * Construct the response
 	 *
 	 * @param int $response_code
 	 * @param string|callable|boolean $message_or_callback
 	 * @param int $code
 	 * @param \Exception $previous
 	 */
-	public function __construct($response_code = 0, $message_or_callback, $code = 0, \Exception $previous = null)
+	public function __construct($response_code = 0, $message_or_callback = '', $code = 0, \Exception $previous = null)
 	{
 		parent::__construct($response_code, $message_or_callback, $code, $previous);
 		// check the bootstrap
